f49d6802371141a8ba5db0fa89d10f6cbc446ac3
iivic17 <ilijaivic17@gmail.com>
2022-11-14 00:23:44 +0100

    Testimonials card positioning fixed

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { quotes } from '../assets';
 
 const FeedbackCard = ({ content, name, title, img }) => (
-	<div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+	<div className='flex justify-between flex-col px-10 py-12 rounded-[20px] flex-1 md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
 		<img
 			src={quotes}
 			alt='double_quotes'
 			className='w-[42px] h-[27px] object-contain mb-10'
 		/>
-		<p className='font-poppins font-normal text-[18px] leading-[32px] text-white mb-[auto]'>
+		<p className='font-poppins font-normal text-[18px] leading-[32px] text-white mb-[auto] max-w-[480px]'>
 			{content}
 		</p>
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -22,7 +22,7 @@ const Testimonials = () => (
 			</div>
 		</div>
 
-		<div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
+		<div className='flex md:flex-row flex-col sm:justify-start justify-center w-full feedback-container relative z-[1]'>
 			{feedback.map(card => (
 				<FeedbackCard key={card.id} {...card} />
 			))}
